1679ec3c513e4cd39a807e2a0c2d0731c3822f18
SamuelLaguna <samanater11@gmail.com>
2022-10-21 11:05:06 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Samuel Laguna
+//10-19-22
+//We are to recreated mini challenge 1. We need to get the same output
+//but using a diffrent method.
+//Peer reivew: 
+
+Console.Clear();
+bool TryAgain = true;
+bool end = false;
+
+while(TryAgain){
+Console.WriteLine("Hello What is your name?");
+
+string myNAme = "";
+bool mn = false;
+
+while(!mn){
+    myNAme = Console.ReadLine();
+Console.WriteLine("My name is " + myNAme);
+bool checkmn = myNAme.All(Char.IsLetter);
+if(checkmn == false)
+{
+    Console.WriteLine("This is not a word");
+    mn = false;
+}else
+{
+    mn = true;
+}
+
+}
+Console.WriteLine("Would you like to try again [TryAgain to restart] [end to end]");
+   string ending = Console.ReadLine();
+     if(ending == "end")
+    {
+        TryAgain = false;
+        end = true;
+    }
+}
